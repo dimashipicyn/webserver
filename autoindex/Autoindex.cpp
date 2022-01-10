0d2f4aaffb0b83edda9ab59aef208814ca8181fe
Ruslan <vaha1st@gmail.com>
2022-01-10 20:25:45 +0300

    Develope autoindex logic

---
+++ autoindex/Autoindex.cpp
@@ -0,0 +1,98 @@
+//
+// Created by Руслан Вахитов on 10.01.2022.
+//
+
+#include "Autoindex.hpp"
+# define FILENAME_LIMIT 50
+
+
+Autoindex::Autoindex(Route const &route) : route_(route){}
+
+std::string Autoindex::formHeader(std::string const &resource)
+{
+	std::string header = std::string("<html>\n")
+							+ "<head><title>Index of "
+							+ resource
+							+ "</title></head>\n"
+							+ "<body bgcolor=\"white\">\n"
+							+ "<h1>Index of "
+							+ resource
+							+ "</h1><hr><pre><a href=\"../\">../</a>";
+	return header;
+}
+
+std::string Autoindex::formBody(const std::string &resource)
+{
+	std::string fullPath = route_.getRoot() + resource;
+	std::string result = "\n";
+	struct stat info;
+	struct dirent *ent;
+	DIR *dir;
+	if ((dir = opendir(fullPath.c_str())) != nullptr) {
+		/* print all the files and directories within directory */
+		while ((ent = readdir (dir)) != nullptr) {
+			std::string tmp = ent->d_name;
+			if (tmp == "." || (tmp[0] == '.' && tmp[1] != 0))
+				continue;
+			stat((fullPath + tmp).c_str(), &info);
+			if (ent->d_type == DT_DIR)
+				tmp += "/";
+			result += std::string("<a href=\"") + tmp + "\">" + formatFilename(tmp)
+					+ "</a>"
+					+ putRemainSpaces(tmp)
+					+ " "
+					+ timespecToUtcString(info.st_mtimespec)
+					+ "\t\t\t"
+					+ (ent->d_type == DT_DIR ? "-" : sizeToString(info.st_size))
+					+ "\n";
+		}
+		closedir (dir);
+	} else {
+		throw std::runtime_error(std::string ("No such directory!") + " \"" + fullPath + "\"");
+	}
+	return result;
+}
+
+std::string Autoindex::formTail()
+{
+	return "</pre><hr></body>\n</html>\n";
+}
+
+std::string Autoindex::generatePage(const std::string &resource)
+{
+	return formHeader(resource) + formBody(resource) + formTail();
+}
+
+std::string Autoindex::timespecToUtcString(timespec const &lastModified)
+{
+	char buf[31];
+	bzero(buf, 31);
+	const int tmpsize = 17;
+	struct tm tm;
+	gmtime_r(&lastModified.tv_sec, &tm);
+	strftime(buf, tmpsize, "%d-%b-%Y %H:%M", &tm);
+	return buf;
+}
+
+std::string Autoindex::sizeToString(const off_t &fileSize)
+{
+	char buf[31];
+	bzero(buf, 31);
+	sprintf(buf, "%lld", fileSize);
+	return buf;
+}
+
+std::string Autoindex::formatFilename(const std::string &filename)
+{
+	return filename.size() > FILENAME_LIMIT ? filename.substr(0, FILENAME_LIMIT - 3) + "..>" : filename;
+}
+
+std::string Autoindex::putRemainSpaces(const std::string &filename)
+{
+	size_t remain = FILENAME_LIMIT > filename.size() ? FILENAME_LIMIT - (filename.size() - 1) : 1;
+	char result[remain];
+	bzero(result, remain);
+	if (remain > 1)
+	memset(result, ' ', remain - 1);
+	return result;
+}
---
+++ autoindex/Autoindex.hpp
@@ -0,0 +1,103 @@
+//
+// Created by Руслан Вахитов on 10.01.2022.
+//
+
+#ifndef WEBSERV_AUTOINDEX_HPP
+#define WEBSERV_AUTOINDEX_HPP
+
+#include <iostream>
+#include <sstream>
+#include <dirent.h>
+#include <sys/stat.h>
+#include "../settingsManager/Route.hpp"
+#include "../utils.h"
+
+class Autoindex
+{
+public:
+	Autoindex(Route const &route);
+
+	/**
+	 * @info Генерирует html страничку для листинга. Скрытые файлы и папки не отображаются. Отображается дата изменения
+	 * и размер файла.
+	 *
+	 * @param resource uri
+	 *
+	 * @return возвращает text/html тело в виде строки
+	 *
+	 * @throws runtime_error если на сервере не существует переданного ресурса
+	 */
+	std::string generatePage(std::string const &resource);
+
+
+private:
+	Route route_;
+
+	/**
+	 * @brief формирует шапку html странички
+	 *
+	 * @param resource uri
+	 *
+	 * @return шапку в виде строки
+	 */
+	std::string formHeader(std::string const &resource);
+
+	/**
+	 * @brief формирует тело html странички с листингом
+	 *
+	 * @param resource uri
+	 *
+	 * @return тело html в виде строки
+	 */
+	std::string formBody(std::string const &resource);
+
+	/**
+	 * @brief формирует конец html странички
+	 *
+	 * @param resource uri
+	 *
+	 * @return строку с закрывающими тегами html
+	 */
+	std::string formTail();
+
+	/**
+	 * @brief конвертирует timespec в строку формата date time UTC
+	 *
+	 * @param lastModified структура секунды + наносекунды
+	 *
+	 * @return строка формата "dd-MMM-yy HH:mm"
+	 */
+	std::string timespecToUtcString(timespec const &lastModified);
+
+	/**
+	 * @brief конвертирует размер файла в строку
+	 *
+	 * @param fileSize - размер файла
+	 *
+	 * @return число строкой
+	 */
+	std::string sizeToString(off_t const &fileSize);
+
+	/**
+	 * @brief форматирует название файла/директории, в случае если длинна названия превышает FILENAME_LIMIT.
+	 *
+	 * @param filename имя файла
+	 *
+	 * @return если название > FILENAME_LIMIT, обрезает его и добавляет "..>" в конец. В обратном случае возвращает
+	 * filename
+	 */
+	std::string formatFilename(std::string const &filename);
+
+	/**
+	 * @brief для названия файла короче FILENAME_LIMIT формирует строку с пробелами, кратной количеству недостающих
+	 * до FILENAME_LIMIT символов
+	 *
+	 * @param filename имя файла
+	 *
+	 * @return строку с проблеами. Если filename > FILENAME_LIMIT то пустую строку
+	 */
+	std::string putRemainSpaces(std::string const &filename);
+};
+
+
+#endif //WEBSERV_AUTOINDEX_HPP
